9e4d9cd09b8d38c906df7e531508771fb8e4044d
VivienShen <vivienshen88@gmail.com>
2021-10-22 21:59:15 -0400

    Created New Todo form and updated the my todo page UI

--- src/components/pages/all-todos/index.jsx
+++ src/components/pages/all-todos/index.jsx
@@ -1,18 +1,19 @@
 import "./styles.css"
 import { Todo } from "../../todo/index";
 
+
+
 export const AllTodosPage = () => {
+    const todos = [
+        {text: "Walk my dog", date: "10/10/2021", color:"blue", isComplete: false },
+        {text: "Do laundry", date: "10/10/2021", color:"red", isComplete: false },
+        {text: "Take test", date: "10/10/2021", color:"red", isComplete: false }
+    ];
+
     return (
-        <div>
-            <Todo
-                text="Walk my dog" date="10/10/2021" color="blue"
-            />
-            <Todo
-                text="Buy laundry detergent" date="10/10/2021" color="blue"
-            />
-            <Todo
-                text="Go workout" date="10/10/2021" color="blue"
-            />
+        <div className="todos-container">
+            { todos.map ((todo) => <Todo text={todo.text} 
+            date={todo.date} color={todo.color} isComplete={todo.isComplete} /> )}
         </div>
     )
 }
